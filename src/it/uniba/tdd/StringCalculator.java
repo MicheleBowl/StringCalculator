02290a052cb855d15d365d7627519deddd5a0a77
Michele <Michele@DESKTOP-6FGU7J6>
2016-11-29 21:18:51 +0100

    little improvement

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -8,35 +8,32 @@ public class StringCalculator {
 		String myNumbersStr = numbersStr;
 		String[] numbers = null;
 		String separator = "\n|,";
-		
-		
-		//preparation of separators
+
+		// preparation of separators
 		if (myNumbersStr.contains("//") && myNumbersStr.contains("\n")) {
 			int end = myNumbersStr.indexOf("\n");
 			String myTempSeparator = myNumbersStr.substring(2, end);
-			int i=0;
+			int i = 0;
 			separator = "";
-			while(i<myTempSeparator.length()){
-				if(myTempSeparator.charAt(i)=='['){
+			while (i < myTempSeparator.length()) {
+				if (myTempSeparator.charAt(i) == '[') {
 					i++;
 				}
-				if(myTempSeparator.charAt(i)==']' && i!=myTempSeparator.length()-1){
+				if (myTempSeparator.charAt(i) == ']' && i != myTempSeparator.length() - 1) {
 					separator += "|";
-				} else if(myTempSeparator.charAt(i)!=']'){
-					separator += myTempSeparator.charAt(i);					
+				} else if (myTempSeparator.charAt(i) != ']') {
+					separator += myTempSeparator.charAt(i);
 				}
 				i++;
 			}
 			myNumbersStr = myNumbersStr.substring(end + 1);
 		}
 
-		//computing of numbers
+		// computing of numbers
 		if (myNumbersStr.length() == 0) {
 			sum = 0;
 		} else {
-			if (myNumbersStr.contains(",\n") || myNumbersStr.contains("\n,")) {
-				throw new StringCalculatorException();
-			}
+			checkIllegalSequence(separator, myNumbersStr);
 			numbers = myNumbersStr.split(separator);
 			for (String num : numbers) {
 				if (num.contains("-")) {
@@ -55,5 +52,19 @@ public class StringCalculator {
 			}
 		}
 		return sum;
+	}
+
+	private void checkIllegalSequence(String separator, String sequence) throws StringCalculatorException {
+		String[] separators = separator.split("\\|");//ci|aa
+		
+		for (int i = 0; i < separators.length; i++) {
+			for (String sep : separators) {
+				if (sequence.contains(separators[i] + sep)) {
+					throw new StringCalculatorException();
+				}
+
+			}
+		}
+
 	}
 }

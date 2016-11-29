6839c9f937bbb68ecedd60fb3953c6d7f3ef2903
Michele <Michele@DESKTOP-6FGU7J6>
2016-11-29 18:21:20 +0100

    implemented all the methods and the test. All function but it need refactoring.

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,9 +1,63 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
 		// not yet implemented
-		return 0;
+		int sum = 0;
+		String myNumbersStr = numbersStr;
+		String[] numbers = null;
+		String separator = "\n|,";
+
+		if (myNumbersStr.contains("//") && myNumbersStr.contains("\n")) {
+			int end = myNumbersStr.indexOf("\n");
+			
+			if (myNumbersStr.contains("//[") && myNumbersStr.contains("]\n")) {
+				if(myNumbersStr.contains("][") ){
+					String tempSeparator = myNumbersStr.substring(3, end -1 );
+					separator = "";
+					for(int i=0; i<tempSeparator.length(); i++){
+						if(tempSeparator.charAt(i)==']' && tempSeparator.charAt(i + 1)=='['){
+							separator += "|";
+						} else if(tempSeparator.charAt(i)!='['){
+							separator += tempSeparator.charAt(i);
+						}
+						
+					}
+					
+				} else {
+					separator = myNumbersStr.substring(3, end -1 );
+				}
+			} else {
+				separator = myNumbersStr.substring(2, 3);
+			}
+			
+			myNumbersStr = myNumbersStr.substring(end + 1);
+		}
+
+		if (myNumbersStr.length() == 0) {
+			sum = 0;
+		} else {
+			if (myNumbersStr.contains(",\n") || myNumbersStr.contains("\n,")) {
+				throw new StringCalculatorException();
+			}
+			numbers = myNumbersStr.split(separator);
+			for (String num : numbers) {
+				if (num.contains("-")) {
+					throw new StringCalculatorException();
+				}
+			}
+
+			if (numbers.length == 1) {
+				sum = Integer.parseInt(numbers[0]);
+			} else {
+				for (String n : numbers) {
+					if (n.length() < 4 || n.equals("1000")) {
+						sum += Integer.parseInt(n);
+					}
+				}
+			}
+		}
+		return sum;
 	}
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -1,14 +1,173 @@
 package it.uniba.tdd.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class StringCalculatorTest {
+import it.uniba.tdd.StringCalculator;
+import it.uniba.tdd.StringCalculatorException;
 
+public class StringCalculatorTest {
+	private String s;
+	private StringCalculator calc;
+
+	@Before
+	public void setUp(){
+		calc = new StringCalculator();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void theSumOfZeroNumbersIsZero() throws StringCalculatorException {
+		// Act
+		s = "";
+		// Assert
+		assertEquals(0, calc.add(s));
+
 	}
 
+	@Test
+	public void theSumOfOneNumbersIsTheNumber() throws StringCalculatorException {
+		// Act
+		s = "100";
+		// Assert
+		assertEquals(100, calc.add(s));
+
+	}
+
+	@Test
+	public void theSumOfTwoNumbersIsTheSum() throws StringCalculatorException {
+		// Act
+		s = "1,22";
+		// Assert
+		assertEquals(23, calc.add(s));
+
+	}
+
+	@Test
+	public void theSumOfTenNumbersIsThemSum() throws StringCalculatorException {
+		// Act
+		s = "1,22,3,24,3,2,1,2,3,2";
+		// Assert
+		assertEquals(63, calc.add(s));
+
+	}
+
+	@Test
+	public void theNumbersCanBeSeparatedAlsoBySlashN() throws StringCalculatorException {
+		// Act
+		s = "1,22\n32";
+		// Assert
+		assertEquals(55, calc.add(s));
+	}
+
+	@Test(expected = StringCalculatorException.class)
+	public void theNumbersCantBeSeparatedByCommaAndSlashN() throws StringCalculatorException {
+		// Act
+		s = "1,\n22";
+		// Assert
+		calc.add(s);
+
+	}
+
+	@Test(expected = StringCalculatorException.class)
+	public void theNumbersCantBeSeparatedBySlashNAndComma() throws StringCalculatorException {
+		// Act
+		s = "1\n,22";
+		// Assert
+		calc.add(s);
+
+	}
+
+	@Test
+	public void theNumbersCanBeSeparatedByAPersonalizedSeparator() throws StringCalculatorException {
+		// Act
+		s = "//;\n1;22;32";
+		// Assert
+		assertEquals(55, calc.add(s));
+	}
+
+	@Test
+	public void theNumbersAreSeparatedByN() throws StringCalculatorException {
+		// Act
+		s = "//N\n5N22N32";
+		// Assert
+		assertEquals(59, calc.add(s));
+	}
+
+	@Test(expected = StringCalculatorException.class)
+	public void ifTheNumbersContainNegativeNumberIsTrhowsAnException() throws StringCalculatorException {
+		// Act
+		s = "//;\n1;-22;32";
+		// Assert
+		calc.add(s);
+
+	}
+
+	@Test
+	public void theNumbersMajorThanOneThousandWithStandardSeparatorAreIgnored() throws StringCalculatorException {
+		// Act
+		s = "1,1022\n32";
+		// Assert
+		assertEquals(33, calc.add(s));
+	}
+
+	@Test
+	public void theNumbersMajorThanOneThousandWithPersonalizedSeparatorAreIgnored() throws StringCalculatorException {
+		// Act
+		s = "//;\n1;1022;32";
+		// Assert
+		assertEquals(33, calc.add(s));
+	}
+
+	@Test
+	public void ifTheNumbersIsOneThousandWithStandardSeparatorIsAccepted() throws StringCalculatorException {
+		// Act
+		s = "1,1000\n32";
+		// Assert
+		assertEquals(1033, calc.add(s));
+	}
+
+	@Test
+	public void ifTheNumbersIsOneThousandWithPersonalizedSeparatorIsAccepted() throws StringCalculatorException {
+		// Act
+		s = "//;\n1;1000;32";
+		// Assert
+		assertEquals(1033, calc.add(s));
+	}
+	
+	@Test
+	public void theNumbersIsSeparatedByAPhraseDelimiter() throws StringCalculatorException {
+		// Act
+		s = "//[ciao]\n1ciao1ciao1";
+		// Assert
+		assertEquals(3, calc.add(s));
+	}
+	
+	@Test
+	public void theNumbersIsSeparatedByTwoPhraseDelimiter() throws StringCalculatorException {
+		// Act
+		s = "//[c][a]\n1c1a1a2c2";
+		// Assert
+		assertEquals(7, calc.add(s));
+	}
+	
+	@Test
+	public void theNumbersIsSeparatedByTwoPhraseDelimiterLongerThanOneChar() throws StringCalculatorException {
+		// Act
+		s = "//[ci][aa]\n1ci1aa1aa2ci2";
+		// Assert
+		assertEquals(7, calc.add(s));
+	}
+	
+	@Test
+	public void theNumbersIsSeparatedByThreePhraseDelimiterLongerThanOneChar2() throws StringCalculatorException {
+		// Act
+		s = "//[ci][aa][,a][-a][è]\n1ci2ci34aa7aa8,a8-a9è";
+		// Assert
+		assertEquals(69, calc.add(s));
+	}
+	
+	
+	
 }
